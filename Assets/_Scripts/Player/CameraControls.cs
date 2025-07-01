1abaeeab353f179468da36fdfe407089b71ecbe2
arstarkir <52779411+arstarkir@users.noreply.github.com>
2025-07-01 06:22:05 -0400

    Camera Controls Rework

--- Assets/_Scripts/Player/CameraControls.cs
+++ Assets/_Scripts/Player/CameraControls.cs
@@ -1,12 +1,14 @@
+using System.Linq;
+using Unity.Netcode;
 using UnityEditor;
 using UnityEngine;
-using Unity.Netcode;
 
 public class CameraControls : NetworkBehaviour
 {
     [SerializeField] AnimationCurve scrollStrengthCurve;
     float scrolingTime = 0;
-    [SerializeField] float borderSize = 3;
+    [SerializeField] Canvas canvas;
+    [SerializeField] RectTransform[] areas;
 
     [HideInInspector] public GameObject active;
 
@@ -41,7 +43,7 @@ public class CameraControls : NetworkBehaviour
         mousePos.x -= Screen.width / 2;
         mousePos.z -= Screen.height / 2;
 
-        if (Mathf.Abs(mousePos.x) > Screen.width / borderSize || Mathf.Abs(mousePos.z) > Screen.height / borderSize)
+        if (!IsMouseInsideAny())
         {
             transform.position += mousePos * 0.01f * scrollStrengthCurve.Evaluate(scrolingTime) * Time.deltaTime;
             scrolingTime += Time.deltaTime;
@@ -50,5 +52,13 @@ public class CameraControls : NetworkBehaviour
         {
             scrolingTime = 0;
         }
+    }
+
+    public bool IsMouseInsideAny()
+    {
+        Vector2 mouse = Input.mousePosition;
+        Camera cam = canvas.renderMode == RenderMode.ScreenSpaceOverlay? null : canvas.worldCamera;
+
+        return areas.Any(rt =>RectTransformUtility.RectangleContainsScreenPoint(rt, mouse, cam));
     }
 }
